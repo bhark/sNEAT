c65025771fe0841ee2aa0409f43ac2c5bb7a2305
Bhark <aeb@tetrabit.coop>
2024-05-31 01:40:10 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sneat',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     package_data={'sneat': ['default_config.ini']},
     install_requires=[
