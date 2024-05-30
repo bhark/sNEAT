02dbc2ab730c4484d3621355fd053dafbae71c67
Bhark <aeb@tetrabit.coop>
2024-05-31 00:28:14 +0200

    better config handling

--- sneat/config.py
+++ sneat/config.py
@@ -14,9 +14,27 @@ activation_functions = {
     'cos': lambda x: np.cos(x),
 }
 
-# config and utilities
 def get_config():
+    """
+    Loads the default configuration and merges it with any user-provided 'config.ini' 
+    in the directory of the executing script.
+    """
+    # initialize the ConfigParser
     config = cp.ConfigParser()
+
+    # path to the default configuration file
     dirpath = os.path.abspath(os.path.dirname(__file__))
-    config.read(f'{dirpath}/default_config.ini')
+    default_config_path = os.path.join(dirpath, 'default_config.ini')
+
+    # read the default configuration
+    config.read(default_config_path)
+
+    # path to a potential user-defined configuration in the execution directory
+    execution_path = os.path.abspath(os.getcwd())
+    user_config_path = os.path.join(execution_path, 'config.ini')
+
+    # check if there's a user config in the execution directory and read it if it exists
+    if os.path.exists(user_config_path):
+        config.read(user_config_path)
+
     return config
