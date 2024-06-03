e4fefae4ce2f5090f8286bbcf32bf1a326c2c9b4
Bhark <aeb@tetrabit.coop>
2024-06-03 17:27:11 +0200

    fix version (jajaja)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='sneat',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     package_data={'sneat': ['default_config.ini']},
     install_requires=[
